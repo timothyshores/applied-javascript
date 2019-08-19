ae114e832a4ba5bcb448b7d8d0336c552546f120
Timothy Shores <timothymshores@gmail.com>
2019-08-18 23:27:14 -0500

    Complete Header/Index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,12 +1,23 @@
-// STEP 1: Create a header component.
-// -----------------------
-// Using a function create the component you see below:
-//
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
-//  </div >
-// And add it to the DOM in the .headerContainer component
+const Header = () => {
+	const div = document.createElement("div");
+	div.classList.add("header");
 
-function Header() {}
+	const date = document.createElement("span");
+	date.classList.add("date");
+	date.textContent = "SMARCH 28, 2019";
+
+	const h1 = document.createElement("h1");
+	h1.textContent = "Lambda Times";
+
+	const temp = document.createElement("span");
+	temp.classList.add("temp");
+	temp.textContent = "98°";
+
+	div.appendChild(date);
+	div.appendChild(h1);
+	div.appendChild(temp);
+
+	return div;
+};
+
+document.querySelector(".header-container").appendChild(Header());
