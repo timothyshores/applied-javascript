da9d92edc794abb06a511667adade51c69c0c7ba
Timothy Shores <timothymshores@gmail.com>
2019-08-19 00:23:55 -0500

    Complete Cards/Index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,47 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const Card = articles => {
+	const cards = articles.map(article => {
+		const card = document.createElement("div");
+		card.classList.add("card");
+
+		const headline = document.createElement("div");
+		headline.classList.add("headline");
+		headline.textContent = article.headline;
+
+		const author = document.createElement("div");
+		author.classList.add("author");
+
+		const imgContainer = document.createElement("div");
+		imgContainer.classList.add("img-container");
+
+		const img = document.createElement("img");
+		img.src = article.authorPhoto;
+
+		const span = document.createElement("span");
+		span.textContent = article.authorName;
+
+		card.appendChild(headline);
+		card.appendChild(author);
+		author.appendChild(imgContainer);
+		imgContainer.appendChild(img);
+		author.appendChild(span);
+
+		return card;
+	});
+
+	return cards;
+};
+
+axios
+	.get("https://lambda-times-backend.herokuapp.com/articles")
+	.then(res => {
+		Object.values(res.data.articles).map(topic =>
+			Card(topic).map(card =>
+				document.querySelector(".cards-container").appendChild(card)
+			)
+		);
+	})
+	.catch(err => console.log(err));
